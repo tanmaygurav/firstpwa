cb38a4fd6f1a780ffcfaccded38ae0d2939d87e7
Tanmay Gurav <tanmayvgurav@gmail.com>
2023-12-13 20:32:08 +0530

    caching version

--- sw.js
+++ sw.js
@@ -1,9 +1,49 @@
-//install service worker
+const staticCacheName = 'site-static-v1';
+const assets = [
+    '/',
+    '/index.html',
+    '/js/app.js',
+    '/js/ui.js',
+    '/js/materialize.min.js',
+    'img/dish.png',
+    'css/styles.css',
+    'css/materialize.min.css',
+    '/manifest.json',
+    'https://fonts.googleapis.com/icon?family=Material+Icons',
+    'https://fonts.gstatic.com/s/materialicons/v140/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2',
+]
+
+// install service worker
 self.addEventListener('install', evt => {
-    console.log("service worker has been installed");
+    //console.log("service worker has been installed");
+    evt.waitUntil(
+        caches.open(staticCacheName).then(cache => {
+            console.log('caching shell assets');
+            cache.addAll(assets)
+        })
+    )
 })
 
 // activate event
-self.addEventListener('activate',evt=>{
-    console.log('service worker has been activated');
+self.addEventListener('activate', evt => {
+    // console.log('service worker has been activated');
+    evt.waitUntil(
+        caches.keys().then(keys => {
+            // console.log("keys",keys);
+            return Promise.all(keys
+                .filter(key => key !== staticCacheName)
+                .map(key => caches.delete(key))
+            )
+        })
+    );
+});
+
+// fetch events
+self.addEventListener('fetch', evt => {
+    // console.log('fetch event',evt);
+    evt.respondWith(
+        caches.match(evt.request).then(cacheRes => {
+            return cacheRes || fetch(evt.request)
+        })
+    )
 })
