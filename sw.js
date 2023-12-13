e0d4f25c043d190ddcf0843f922c0d851d2b2e2e
Tanmay Gurav <tanmayvgurav@gmail.com>
2023-12-13 20:38:14 +0530

    dynamic caching

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
 const staticCacheName = 'site-static-v1';
+const dynamicCache = "dynamicCache-v1";
 const assets = [
     '/',
     '/index.html',
@@ -21,8 +22,8 @@ self.addEventListener('install', evt => {
             console.log('caching shell assets');
             cache.addAll(assets)
         })
-    )
-})
+    );
+});
 
 // activate event
 self.addEventListener('activate', evt => {
@@ -43,7 +44,12 @@ self.addEventListener('fetch', evt => {
     // console.log('fetch event',evt);
     evt.respondWith(
         caches.match(evt.request).then(cacheRes => {
-            return cacheRes || fetch(evt.request)
+            return cacheRes || fetch(evt.request).then(fetchRes => {
+                return caches.open(dynamicCache).then(cache => {
+                    cache.put(evt.request.url, fetchRes.clone());
+                    return fetchRes;
+                })
+            })
         })
-    )
-})
+    );
+});
